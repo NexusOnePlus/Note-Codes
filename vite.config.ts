19fb6decad971cf77808ea7cd094f06f4ce67465
Will <wv35788@gmail.com>
2024-11-10 20:14:11 -0500

    one more time

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import deno from '@deno/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from 'tailwindcss'
+import process from "node:process"
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [deno(), react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+
+    plugins: [deno(), react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      }
+    },
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
     }
   }
 })
